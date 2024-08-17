350a9a529edd6fc40e5fae33fd771ab967c3d243
Abdullah <Abdullah500dev@gmail.com>
2024-08-17 17:46:00 +0500

    new changes

--- app/api/seeker-users/route.js
+++ app/api/seeker-users/route.js
@@ -6,7 +6,7 @@ export async function GET() {
   try {
     await connectDb(); // Ensure the DB is connected
 
-    const users = await User.find({ employer: { $regex: '^seeker$', $options: 'i' } });
+    const users = await User.find({ employer: { $regex: 'seeker', $options: 'i' } });
 
     await disconnectDb(); // Disconnect after the query is done
 
