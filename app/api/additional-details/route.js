c78b67f4b8593c2671ad0b7d715adf9ad5a5b6e6
Abdullah <Abdullah500dev@gmail.com>
2024-08-21 22:23:18 +0500

    cloudinary add

--- app/api/additional-details/route.js
+++ app/api/additional-details/route.js
@@ -1,49 +1,19 @@
 import { NextResponse } from "next/server";
 import { connectDb } from "../../lib/mongo/conectDB";
 import User from "../../lib/mongo/schema/userSchema";
-import cloudinary from "cloudinary";
 
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
-
-export async function POST(request) {   
+export async function POST(request) {
   await connectDb();
 
   try {
-    const formData = await request.formData();
-    
-    const email = formData.get("email");
-    const description = formData.get("description");
-    const imageFile = formData.get("image");
+    const { email, description, imageUrl } = await request.json();
 
-    if (!email || !description || !imageFile) {
+    if (!email || !description || !imageUrl) {
       return NextResponse.json(
         { error: "All fields are required." },
         { status: 400 }
       );
     }
-
-    // Convert the file to a buffer
-    const arrayBuffer = await imageFile.arrayBuffer(); // Fixed typo here
-    const buffer = Buffer.from(arrayBuffer);
-
-    const result = await new Promise((resolve, reject) => {
-      const cloudinaryStream = cloudinary.v2.uploader.upload_stream(
-        { folder: "user_images" },
-        (error, result) => {
-          if (error) {
-            reject(new Error("Image upload failed"));
-          } else {
-            resolve(result.secure_url);
-          }
-        }
-      );
-
-      cloudinaryStream.end(buffer);
-    });
 
     const user = await User.findOne({ email });
 
@@ -53,8 +23,8 @@ export async function POST(request) {
 
     // Updating user fields
     user.description = description;
-    user.imageUrl = result;
-    await user.save(); // Updated to save user directly
+    user.imageUrl = imageUrl;
+    await user.save(); // Save user directly
 
     return NextResponse.json(
       { message: "Profile updated successfully", user },
