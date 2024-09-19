e11f308182adb7cf0431bd6fcdf8082d33682ce0
Tekvillgithubrit <aliahmadbutt878@gmail.com>
2024-09-19 23:13:22 +0500

    get-applied-user --no change

--- app/api/get-applied-user/route.js
+++ app/api/get-applied-user/route.js
@@ -47,45 +47,4 @@ export async function GET(request) {
   } catch (error) {
     return NextResponse.json({ message: "Error fetching users", error: error.message });
   }
-
-    // Step 1: Find all jobs created by the logged-in user
-    // const jobsCreatedByUser = await Job.find({ user: userId });
-
-    // Extract job IDs created by the user
-    // const jobIds = jobsCreatedByUser.map((job) => job._id);
-
-    // // Step 2: Find all users who applied for these jobs
-    // const appliedUsers = await User.aggregate([
-    //   {
-    //     $match: {
-    //       jobapllied: { $in: jobIds }, // Match users whose 'jobapllied' contains any of the job IDs created by the user
-    //     },
-    //   },
-    //   {
-    //     $lookup: {
-    //       from: "jobs", // Ensure this matches the actual collection name in MongoDB
-    //       localField: "jobapllied",
-    //       foreignField: "_id",
-    //       as: "appliedJobs",
-    //     },
-    //   },
-    //   {
-    //     $unwind: "$appliedJobs" // Unwind the appliedJobs array to create a separate document for each job
-    //   },
-    //   {
-    //     $project: {
-    //       userName: 1,
-    //       email: 1,
-    //       _id: 1,
-    //       imageUrl: 1, // Include the user's image URL
-    //       "appliedJobs.jobTitle": 1, // Include job titles from appliedJobs
-    //       "appliedJobs._id": 1, // Include job IDs from appliedJobs
-    //     },
-    //   },
-    // ]);
-
-  //   return NextResponse.json(appliedUsers);
-  // } catch (error) {
-  //   return NextResponse.json({ message: "Error fetching users", error: error.message });
-  // }
 }
