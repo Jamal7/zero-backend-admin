7fbaabb99393e8613c76646ec9edd7d4bb64c04b
esha-arif <eshaarif0322@gmail.com>
2024-09-24 18:55:15 +0500

    change the funvtionality

--- app/components/adminCompnents/employer/employe.js
+++ app/components/adminCompnents/employer/employe.js
@@ -311,7 +311,7 @@ const EditModal = ({ seeker, onClose, onSave }) => {
             </button>
             <button
               type="submit"
-              className="px-4 py-2 bg-blue-500 text-white rounded-md"
+              className="px-4 py-2 bg-blue-600 text-white rounded-md"
               disabled={loading}
             >
               Save
